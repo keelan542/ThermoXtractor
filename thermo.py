f2b58f44d60ef76ac1bede8f1dd88cff404fe0c5
keelan542 <keelanb542@gmail.com>
2023-06-21 15:25:12 +0100

    Added thermochemistry data to a list

--- thermo.py
+++ thermo.py
@@ -4,6 +4,9 @@ import sys
 # Found thermo flag
 found_thermo = False
 
+# Thermo data list
+thermo_data = []
+
 # Open file and read line by line
 try:
 	with open(sys.argv[1]) as file:
@@ -13,9 +16,12 @@ try:
 			elif ' Sum of electronic and thermal Free Energies=' in line:
 				found_thermo = False
 				print(line)
-
-			if found_thermo:
+				thermo_data.append(line.split()[-1])
+			elif found_thermo:
 				print(line)
+				thermo_data.append(line.split()[-1])
+
+	print(thermo_data)
 
 except Exception:
 	print('File not found.')
