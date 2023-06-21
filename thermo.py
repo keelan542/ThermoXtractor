885edd0b53a643abbdb3e1febb015a9b9d0c1ad3
keelan542 <keelanb542@gmail.com>
2023-06-21 15:09:54 +0100

    Add code to locate thermochemistry corrections

--- thermo.py
+++ thermo.py
@@ -1,12 +1,22 @@
 # Import statements
 import sys
 
+# Found thermo flag
+found_thermo = False
+
 # Open file and read line by line
 try:
 	with open(sys.argv[1]) as file:
 		for line in file:
-			if ' - Thermochemistry -' in line:
-				print('Found Thermochemistry')
+			if ' Zero-point correction=' in line:
+				found_thermo = True
+			elif ' Sum of electronic and thermal Free Energies=' in line:
+				found_thermo = False
+				print(line)
+
+			if found_thermo:
+				print(line)
+
 except Exception:
 	print('File not found.')
 	sys.exit()
