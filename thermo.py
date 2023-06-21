15321695b6690b968cd66d266c0d79becb37775d
keelan542 <keelanb542@gmail.com>
2023-06-21 16:06:23 +0100

    Removed some print statements, and formatted thermochemistry into easy to read string

--- thermo.py
+++ thermo.py
@@ -1,5 +1,6 @@
 # Import statements
 import sys
+import math
 
 # Found thermo flag
 found_thermo = False
@@ -13,16 +14,24 @@ try:
 		for line in file:
 			if ' Zero-point correction=' in line:
 				found_thermo = True
+				thermo_data.append(float(line.split()[-2]))
 			elif ' Sum of electronic and thermal Free Energies=' in line:
 				found_thermo = False
-				print(line)
-				thermo_data.append(line.split()[-1])
+				thermo_data.append(float(line.split()[-1]))
 			elif found_thermo:
-				print(line)
-				thermo_data.append(line.split()[-1])
+				thermo_data.append(float(line.split()[-1]))
 
-	print(thermo_data)
+	# Building formatted string so that thermochemistry can be displayed in an easy to read manner
+	formatted_data = 'E(Electronic)\tE(+ZPE)\t\tE(Enthalpy)\tE(Gibbs)\tE(ZPVE)\t\tE(G0>298)\n{}\t{}\t{}\t{}\t{}\t{}'
 
-except Exception:
+	# Inserting approporiate data into formatted string, formatted_data and printing
+	print(formatted_data.format(round(thermo_data[4] - thermo_data[0], 6),
+								round(thermo_data[4], 6),
+								round(thermo_data[6], 6),
+								round(thermo_data[7], 6),
+								round(thermo_data[0], 6),
+								round(thermo_data[3], 6)))
+
+except Exception as e:
 	print('File not found.')
 	sys.exit()
