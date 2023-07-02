13b8c7d4ed0b624717e731113455f42ace4ca759
keelan542 <keelanb542@gmail.com>
2023-07-02 16:33:22 +0100

    Tab not working sometimes has been fixed

--- thermo.py
+++ thermo.py
@@ -37,12 +37,12 @@ try:
 						thermo_data.append(float(line.split()[-1]))
 
 			# Building formatted string so that thermochemistry can be displayed in an easy to read manner
-			formatted_data = '{:30s}{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}'
+			formatted_data = '{:30s}{:.6f} \t{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}'
 
 			# Inserting approporiate data into formatted string, formatted_data and printing
 			current_file = current_file[:-4]
-			if len(current_file) > 20:
-				current_file = current_file[:20]
+			if len(current_file) > 30:
+				current_file = current_file[:30]
 
 			if len(thermo_data) > 0:
 				print(formatted_data.format(current_file,
@@ -56,5 +56,5 @@ try:
 				print(formatted_data.format(current_file, energies[-1], 0.0, 0.0, 0.0, 0.0, 0.0))
 
 except Exception:
-	print('An error has occured.')
+	print('No .log files found.')
 	sys.exit()
